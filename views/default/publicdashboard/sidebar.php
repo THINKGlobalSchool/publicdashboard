21c0d41697d6526b3e1199e7a2229b34ee6d13b9
Jeff Tilson <jrtilson@gmail.com>
2011-08-09 15:19:49 +0000

    Shuffling tags for public tags sidebar

--- views/default/publicdashboard/sidebar.php
+++ views/default/publicdashboard/sidebar.php
@@ -18,22 +18,25 @@ $stats_header = elgg_get_plugin_setting('statstitle', 'publicdashboard');
 $stats_content = elgg_view('publicdashboard/site_stats');
 
 // Public Tags content
-elgg_set_context('tags');
-$tags_content = elgg_view_tagcloud(array(
+$tags = elgg_get_tags(array(
 	'threshold' => elgg_get_plugin_setting('tagthreshold', 'publicdashboard'), 
 	'limit' => elgg_get_plugin_setting('taglimit', 'publicdashboard')
 ));
-elgg_set_context($context);
+
+// Shuffle tags
+shuffle($tags);
+
+$tags_content = elgg_view("output/tagcloud", array('value' => $tags));
 
 if ($tags_content) {
 	$tags_header = elgg_echo('publicdashboard:tagcloud');
 	$context = elgg_get_context();
+	$tags = elgg_view_module('aside', $tags_header, $tags_content);
 }
 
 // Display content in modules for consistency 
 $intro = elgg_view_module('aside', $intro_header, $intro_content);
 $stats = elgg_view_module('aside', $stats_header, $stats_content);
-$tags = elgg_view_module('aside', $tags_header, $tags_content);
 
 $content = <<<HTML
 	<div class='publicdashboard'>
